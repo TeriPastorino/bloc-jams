72bb8d309e9ff11896ac56d3252088d191b80d4d
Teri Pastorino <tpasto@comcast.net>
2015-09-01 15:57:33 -0700

    refactor script revealPoints function and loop

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,30 @@
- var animatePoints = function() {
+//var animatePoints = function() {
 
-   var points = document.getElementsByClassName('point');
+  var points = document.getElementsByClassName('point');
 
+  var revealPoints = function (points) {
+    for (var i = 0; i < points.length; i++) {
+      points[i].style.opacity = 1;
+      points[i].style.transform = "scaleX(1) translateY(0)";
+      points[i].style.msTransform = "scaleX(1) translateY(0)";
+      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+  };
+
+  revealPoints();
+
+
+//refactor these to be a single function called revealPoints that takes a single argument points
+//need for loop 
+//look up tutorial from codecademy
+//lookup last ex in jsbin
+//loop through and assign the formatting to each one?
+// call revealPoints
+      
+      
+      
+      
+  /*
    var revealFirstPoint = function() {
        points[0].style.opacity = 1;
        points[0].style.transform = "scaleX(1) translateY(0)";
@@ -29,4 +52,5 @@
 
 };
 
-animatePoints();
+animatePoints();*/
+   
