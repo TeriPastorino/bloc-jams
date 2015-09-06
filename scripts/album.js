a99b074f5cd03308c37e09e5d832a6fa61ca6c87
Teri Pastorino <tpasto@comcast.net>
2015-09-06 16:47:52 -0700

    add event listener click through album image

--- scripts/album.js
+++ scripts/album.js
@@ -30,45 +30,79 @@ var albumMarconi = {
  ]
 };
 
-var createSongRow = function(songNumber, songName, songLength) {
-  
-  var template =
-        '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
-      + '  <td class="song-item-title">' + songName + '</td>'
-      + '  <td class="song-item-duration">' + songLength + '</td>'
-      + '</tr>'
-      ;
- 
-     return template;
+var albumTolkien = {
+  name: 'The Hobbit',
+  author: 'J.R.R. Tolkien',
+  label: 'Mariner',
+  year: '1937',
+  albumArtUrl: 'assets/images/album_covers/09.png',
+  songs: [
+    { name: 'A short rest', length: '2:01' },
+    { name: 'Over Hill and Under Hill', length: '3:01' },
+    { name: 'Flies and Spiders', length: '3:44'},
+    { name: 'The Return Journey', length: '5:14' },
+    { name: 'Not at Home', length: '2:45'} 
+  ]
 };
 
-var setCurrentAlbum = function(album) {
-  //create variable and get items by Class Name 
+var createSongRow = function(songNumber, songName, songLength) {
+     
+   var template =
+      '<tr class="album-view-song-item">'
+    + '  <td class="song-item-number">' + songNumber + '</td>'
+    + '  <td class="song-item-title">' + songName + '</td>'
+    + '  <td class="song-item-duration">' + songLength + '</td>'
+    + '</tr>'
+    ;
+
+   return template;
+ 
+ };
   var albumTitle = document.getElementsByClassName('album-view-title')[0];
   var albumArtist = document.getElementsByClassName('album-view-artist')[0];
   var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
   var albumImage = document.getElementsByClassName('album-cover-art')[0];
-  var albumSongList = document.getElementsByClassName('album-view-song-list')[0]; 
+  var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ 
+var setCurrentAlbum = function(album) {
+ 
+     // #1
+     
+     // #2
+     albumTitle.firstChild.nodeValue = album.name;
+     albumArtist.firstChild.nodeValue = album.artist;
+     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+     albumImage.setAttribute('src', album.albumArtUrl);
+ 
+     // #3
+     albumSongList.innerHTML = '';
+ 
+     // #4
+     for (i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
+     }
+ 
+ };
+ 
+ window.onload = function() {
+   
+   setCurrentAlbum(albumPicasso);   
+   var albumCoverList = [albumPicasso, albumMarconi,albumTolkien];
+   //set up for loop
+   var i = 1
+   //add click listener event to albumCoverList var
+   albumImage.addEventListener('click', function(event) {
+     //setup function(event)
+     setCurrentAlbum(albumCoverList[i]);
+     i++;
+     if (index == albumCoverList.length) {
+       index = 0;
+     }  
+   });   
+ };  
+                                                                         ;
   
-  //first Child and NodeValue
-  
-  albumTitle.firstChild.nodeValue = album.name;
-  albumArtist.firstChild.nodeValue = album.artist;
-  albumReleaseInfo.firstChild.nodeValue = album.year + ' ' album.label;
-  albumImage.setAttribute('src', album.albumArtUrl);
-  
-  albumSongList.innerHTML = '';
-  
-  for (i= 0; i < album.songs.length; i++) {
-    albumSongList.innerHTML += createSongRow(i +1, album.songs[i].name, album.songs[i].length);
-  }
-  
-};
 
-window.onload = function() {
-  setCurrentAlbum(albumPicasso);
-};
   
   
   
@@ -83,7 +117,3 @@ window.onload = function() {
   
   
   
-  
-  
-  
-}
